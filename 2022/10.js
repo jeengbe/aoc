7afc98ae21bf00bbc0d534233a18edfddf840f91
Jesper Engberg <jeengbe@gmail.com>
2022-12-10 16:19:09 +0100

    2022-10

---
+++ 2022/10.js
@@ -0,0 +1,384 @@
+function doCycle1(strengths, regX, c) {
+  if ((c - 20) % 40 === 0) {
+    strengths.push(regX * c);
+  }
+}
+
+export function part1(input) {
+  input = [...input];
+  let regX = 1;
+  const strengths = [];
+  for (let c = 1; input.length; ) {
+    const [cmd, val] = input.shift().split(" ");
+    switch (cmd) {
+      case "addx":
+        c++;
+        doCycle1(strengths, regX, c);
+        c++;
+        regX += +val;
+        break;
+      case "noop":
+        c++;
+        break;
+      default:
+        throw new Error("Unknown command");
+    }
+    doCycle1(strengths, regX, c);
+  }
+  return strengths.reduce((sum, s) => sum + s, 0);
+}
+
+export const tests1 = [
+  [
+    [
+      "addx 15",
+      "addx -11",
+      "addx 6",
+      "addx -3",
+      "addx 5",
+      "addx -1",
+      "addx -8",
+      "addx 13",
+      "addx 4",
+      "noop",
+      "addx -1",
+      "addx 5",
+      "addx -1",
+      "addx 5",
+      "addx -1",
+      "addx 5",
+      "addx -1",
+      "addx 5",
+      "addx -1",
+      "addx -35",
+      "addx 1",
+      "addx 24",
+      "addx -19",
+      "addx 1",
+      "addx 16",
+      "addx -11",
+      "noop",
+      "noop",
+      "addx 21",
+      "addx -15",
+      "noop",
+      "noop",
+      "addx -3",
+      "addx 9",
+      "addx 1",
+      "addx -3",
+      "addx 8",
+      "addx 1",
+      "addx 5",
+      "noop",
+      "noop",
+      "noop",
+      "noop",
+      "noop",
+      "addx -36",
+      "noop",
+      "addx 1",
+      "addx 7",
+      "noop",
+      "noop",
+      "noop",
+      "addx 2",
+      "addx 6",
+      "noop",
+      "noop",
+      "noop",
+      "noop",
+      "noop",
+      "addx 1",
+      "noop",
+      "noop",
+      "addx 7",
+      "addx 1",
+      "noop",
+      "addx -13",
+      "addx 13",
+      "addx 7",
+      "noop",
+      "addx 1",
+      "addx -33",
+      "noop",
+      "noop",
+      "noop",
+      "addx 2",
+      "noop",
+      "noop",
+      "noop",
+      "addx 8",
+      "noop",
+      "addx -1",
+      "addx 2",
+      "addx 1",
+      "noop",
+      "addx 17",
+      "addx -9",
+      "addx 1",
+      "addx 1",
+      "addx -3",
+      "addx 11",
+      "noop",
+      "noop",
+      "addx 1",
+      "noop",
+      "addx 1",
+      "noop",
+      "noop",
+      "addx -13",
+      "addx -19",
+      "addx 1",
+      "addx 3",
+      "addx 26",
+      "addx -30",
+      "addx 12",
+      "addx -1",
+      "addx 3",
+      "addx 1",
+      "noop",
+      "noop",
+      "noop",
+      "addx -9",
+      "addx 18",
+      "addx 1",
+      "addx 2",
+      "noop",
+      "noop",
+      "addx 9",
+      "noop",
+      "noop",
+      "noop",
+      "addx -1",
+      "addx 2",
+      "addx -37",
+      "addx 1",
+      "addx 3",
+      "noop",
+      "addx 15",
+      "addx -21",
+      "addx 22",
+      "addx -6",
+      "addx 1",
+      "noop",
+      "addx 2",
+      "addx 1",
+      "noop",
+      "addx -10",
+      "noop",
+      "noop",
+      "addx 20",
+      "addx 1",
+      "addx 2",
+      "addx 2",
+      "addx -6",
+      "addx -11",
+      "noop",
+      "noop",
+      "noop",
+    ],
+    13140,
+  ],
+];
+
+function doTick2(screen, regX, c) {
+  if (Math.abs(((c - 1) % 40) - regX) <= 1) {
+    screen.push("#");
+  } else {
+    screen.push(".");
+  }
+}
+
+const width = 40;
+const height = 6;
+function drawScreen(screen) {
+  let str = [""];
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      str[str.length - 1] += screen[y * width + x];
+    }
+    str.push("");
+  }
+  return str;
+}
+
+export function part2(input) {
+  input = [...input];
+  let x = 1;
+  const screen = [];
+  for (let c = 0; input.length; ) {
+    const [cmd, val] = input.shift().split(" ");
+    switch (cmd) {
+      case "addx":
+        c++;
+        doTick2(screen, x, c);
+        c++;
+        doTick2(screen, x, c);
+        x += +val;
+        break;
+      case "noop":
+        c++;
+        doTick2(screen, x, c);
+        break;
+      default:
+        throw new Error("Unknown command");
+    }
+  }
+  console.log(drawScreen(screen));
+  return null;
+}
+
+export const tests2 = [
+  [
+    [
+      "addx 15",
+      "addx -11",
+      "addx 6",
+      "addx -3",
+      "addx 5",
+      "addx -1",
+      "addx -8",
+      "addx 13",
+      "addx 4",
+      "noop",
+      "addx -1",
+      "addx 5",
+      "addx -1",
+      "addx 5",
+      "addx -1",
+      "addx 5",
+      "addx -1",
+      "addx 5",
+      "addx -1",
+      "addx -35",
+      "addx 1",
+      "addx 24",
+      "addx -19",
+      "addx 1",
+      "addx 16",
+      "addx -11",
+      "noop",
+      "noop",
+      "addx 21",
+      "addx -15",
+      "noop",
+      "noop",
+      "addx -3",
+      "addx 9",
+      "addx 1",
+      "addx -3",
+      "addx 8",
+      "addx 1",
+      "addx 5",
+      "noop",
+      "noop",
+      "noop",
+      "noop",
+      "noop",
+      "addx -36",
+      "noop",
+      "addx 1",
+      "addx 7",
+      "noop",
+      "noop",
+      "noop",
+      "addx 2",
+      "addx 6",
+      "noop",
+      "noop",
+      "noop",
+      "noop",
+      "noop",
+      "addx 1",
+      "noop",
+      "noop",
+      "addx 7",
+      "addx 1",
+      "noop",
+      "addx -13",
+      "addx 13",
+      "addx 7",
+      "noop",
+      "addx 1",
+      "addx -33",
+      "noop",
+      "noop",
+      "noop",
+      "addx 2",
+      "noop",
+      "noop",
+      "noop",
+      "addx 8",
+      "noop",
+      "addx -1",
+      "addx 2",
+      "addx 1",
+      "noop",
+      "addx 17",
+      "addx -9",
+      "addx 1",
+      "addx 1",
+      "addx -3",
+      "addx 11",
+      "noop",
+      "noop",
+      "addx 1",
+      "noop",
+      "addx 1",
+      "noop",
+      "noop",
+      "addx -13",
+      "addx -19",
+      "addx 1",
+      "addx 3",
+      "addx 26",
+      "addx -30",
+      "addx 12",
+      "addx -1",
+      "addx 3",
+      "addx 1",
+      "noop",
+      "noop",
+      "noop",
+      "addx -9",
+      "addx 18",
+      "addx 1",
+      "addx 2",
+      "noop",
+      "noop",
+      "addx 9",
+      "noop",
+      "noop",
+      "noop",
+      "addx -1",
+      "addx 2",
+      "addx -37",
+      "addx 1",
+      "addx 3",
+      "noop",
+      "addx 15",
+      "addx -21",
+      "addx 22",
+      "addx -6",
+      "addx 1",
+      "noop",
+      "addx 2",
+      "addx 1",
+      "noop",
+      "addx -10",
+      "noop",
+      "noop",
+      "addx 20",
+      "addx 1",
+      "addx 2",
+      "addx 2",
+      "addx -6",
+      "addx -11",
+      "noop",
+      "noop",
+      "noop",
+    ],
+    null,
+  ],
+];
